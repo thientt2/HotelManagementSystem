d8b6c369fc3ade074ca8057d9f35817ae7fe1c16
thientt2 <tt219007@gmail.com>
2024-04-20 22:30:39 +0700

    Update MainView

--- src/UI/MainWindow_Controller.java
+++ src/UI/MainWindow_Controller.java
@@ -1,5 +1,87 @@
 package UI;
 
-public class MainWindow_Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class MainWindow_Controller implements Initializable {
+
+	@FXML
+    private Button baoCao_btn;
+
+    @FXML
+    private Button caiDat_btn;
+
+    @FXML
+    private TableColumn<?, ?> colGia;
+
+    @FXML
+    private TableColumn<?, ?> colNguoiToiDa;
+
+    @FXML
+    private TableColumn<?, ?> colTenLoai;
+
+    @FXML
+    private Button dichVu_btn;
+
+    @FXML
+    private Button hoaDon_btn;
+
+    @FXML
+    private Button khachHang_btn;
+
+    @FXML
+    private Button nhanPhong_btn;
+
+    @FXML
+    private Button nhanVien_btn;
+
+    @FXML
+    private Button phong_btn;
+
+    @FXML
+    private AnchorPane phong_form;
+
+    @FXML
+    private Button suaLoai_btn;
+
+    @FXML
+    private Button suaPhong_btn;
+
+    @FXML
+    private Button themLoaiP_btn;
+
+    @FXML
+    private Button themPhong_btn;
+
+    @FXML
+    private TextField timKiemPhong;
+
+    @FXML
+    private Label toDay;
+
+    @FXML
+    private Button trangChu_btn;
+
+    @FXML
+    private Label username;
+
+    @FXML
+    private Button xoaLoaiP_btn;
+
+    @FXML
+    private Button xoaPhong_btn;
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
