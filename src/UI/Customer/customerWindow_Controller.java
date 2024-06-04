8ec973f836dbdd2487d3d05676a85d2a27d19556
bone204 <truongbmt4@gmail.com>
2024-06-04 22:18:09 +0700

    sua css

--- src/UI/Customer/customerWindow_Controller.java
+++ src/UI/Customer/customerWindow_Controller.java
@@ -12,6 +12,9 @@ import DTO.KHACHHANG;
 import DTO.NHANVIEN;
 import UI.MainWindow_Controller;
 import UI.Resource.itemCustomer_Controller;
+import UI.Resource.itemStaff_Controller;
+import UI.Staff.editStaff_Controller;
+import UI.Staff.staffDetails_Controller;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -38,11 +41,21 @@ public class customerWindow_Controller implements Initializable {
 
     @FXML
     private VBox listCustomer_vbox;
+    
+    @FXML
+    private Pagination pagination;
 
     @FXML
     private TextField searchCustomer;
     
     private MainWindow_Controller mainWindowController;
+    
+    private static final int ITEMS_PER_PAGE = 8;
+    private ObservableList<KHACHHANG> listCustomer= FXCollections.observableArrayList();
+    
+    private int calculatePageCount() {
+        return (int) Math.ceil((double) listCustomer.size() / ITEMS_PER_PAGE);
+    }
 
     // Phương thức để thiết lập tham chiếu của main window controller từ bên ngoài
     public void setMainWindowController(MainWindow_Controller controller) {
@@ -51,6 +64,119 @@ public class customerWindow_Controller implements Initializable {
     
     public MainWindow_Controller getMainWindowController() {
 		return mainWindowController;
+	}
+    
+    private VBox createPage(int pageIndex) {	    
+	    listCustomer_vbox.getChildren().clear(); // Clear old items
+
+	    int startIndex = pageIndex * ITEMS_PER_PAGE;
+	    int endIndex = Math.min(startIndex + ITEMS_PER_PAGE, listCustomer.size());
+
+	    for (int i = startIndex; i < endIndex; i++) {
+	        KHACHHANG customer = listCustomer.get(i);	        
+	        try {
+				FXMLLoader loader = new FXMLLoader(getClass().getResource("/UI/Resource/itemCustomer.fxml"));
+				Parent customerDataPane = loader.load();
+				itemCustomer_Controller controller = loader.getController();
+				controller.setCustomer(customer);
+				Button contextMenu_btn = controller.getContextMenu_btn();
+	            contextMenu_btn.setOnMouseClicked(event -> {
+	            	if (contextMenu.isShowing()) {
+	                    contextMenu.hide();
+	                    return;
+	                }
+	                contextMenu.getItems().clear();
+
+	                MenuItem editItem = new MenuItem("Chỉnh sửa");
+	                MenuItem deleteItem = new MenuItem("Xóa");
+	                MenuItem detailItem = new MenuItem("Chi tiết");
+
+	                editItem.setOnAction(eventEditCustomer -> {
+	                    try {
+	                    	AnchorPane anchorPane = mainWindowController.getAnchorPane();
+	                	    anchorPane.setVisible(true);
+	                	    
+	                        FXMLLoader loader2 = new FXMLLoader(getClass().getResource("editCustomer.fxml"));
+	                        Parent root = loader2.load();
+
+	                        root.setOnMousePressed((MouseEvent event1) -> {            
+	                            x = event1.getSceneX();            
+	                            y = event1.getSceneY();    
+	                        });
+	                        
+	                        Stage stage = new Stage();        
+	                        stage.initStyle(StageStyle.TRANSPARENT);        
+	                        Scene scene = new Scene(root);
+
+	                        editCustomer_Controller editCustomer = loader2.getController();
+	                        editCustomer.setCustomer(customer);
+
+	                        root.setOnMouseDragged((MouseEvent event1) -> {
+	                            stage.setX(event1.getScreenX() - x);            
+	                            stage.setY(event1.getScreenY() - y);       
+	                        });
+	                        
+	                        stage.setScene(scene);     
+	                        stage.showAndWait();
+	                        
+	                        anchorPane.setVisible(false);
+	                        //refreshCustomerList();
+	                    } catch (IOException e) {
+	                        e.printStackTrace();
+	                    }
+	                });
+	        	    
+	        	    deleteItem.setOnAction(deleteEvent -> {
+	        	    	KHACHHANG_BLL.deleteCustomer(customer);
+	        	    	refreshCustomerList();
+	        	    });
+	        	    
+	        	    detailItem.setOnAction(detailEvent -> {
+	        	    	try {
+	                    	AnchorPane anchorPane = mainWindowController.getAnchorPane();
+	                	    anchorPane.setVisible(true);
+	                	    
+	                        FXMLLoader loader1 = new FXMLLoader(getClass().getResource("customerDetails.fxml"));
+	                        Parent root = loader1.load();
+
+	                        root.setOnMousePressed((MouseEvent event1) -> {            
+	                            x = event1.getSceneX();            
+	                            y = event1.getSceneY();    
+	                        });
+	                        
+	                        Stage stage = new Stage();        
+	                        stage.initStyle(StageStyle.TRANSPARENT);        
+	                        Scene scene = new Scene(root);
+
+	                        customerDetails_Controller customerDetails = loader1.getController();
+	                        customerDetails.setCustomer(customer);
+
+	                        root.setOnMouseDragged((MouseEvent event1) -> {
+	                            stage.setX(event1.getScreenX() - x);            
+	                            stage.setY(event1.getScreenY() - y);       
+	                        });
+	                        
+	                        stage.setScene(scene);     
+	                        stage.showAndWait();
+	                        
+	                        anchorPane.setVisible(false);
+	                        //refreshCustomerList();
+	                    } catch (IOException e) {
+	                        e.printStackTrace();
+	                    }
+	        	    });
+	        	    
+	        	    contextMenu.getItems().addAll(editItem, deleteItem, detailItem);  	
+	        	    contextMenu.show(contextMenu_btn, event.getScreenX(), event.getScreenY());
+	            });
+	            Platform.runLater(() -> listCustomer_vbox.getChildren().add(customerDataPane));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	
+	    }
+
+	    return new VBox(); // Return an empty VBox as a placeholder
 	}
     
 	private double x = 0;
@@ -227,18 +353,9 @@ public class customerWindow_Controller implements Initializable {
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
 		
-		Task<ObservableList<KHACHHANG>> task = new Task<>() {			
-			@Override
-			protected ObservableList<KHACHHANG> call() throws Exception {
-				return KHACHHANG_BLL.listCustomer();
-			}			
-		};
-		
-		task.setOnSucceeded(event -> {
-			ObservableList<KHACHHANG> listCustomer = task.getValue();
-			showListCustomer(listCustomer);
-		});
-		new Thread(task).start();
+		listCustomer = KHACHHANG_BLL.listCustomer();
+		pagination.setPageCount(calculatePageCount());
+	    pagination.setPageFactory(this::createPage);	
 
 		search();
 	}
